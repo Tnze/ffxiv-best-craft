ca875cf09de8d6798c68405ebb5601a4e842dbb7
Tnze <cjd001113@outlook.com>
2022-05-10 00:31:38 +0800

    blockly 2

---
+++ blockly-extensions.d.ts
@@ -0,0 +1,2 @@
+declare module '@blockly/theme-modern';
+declare module 'blockly/msg/zh-hans';
--- blockly-theme-modern.d.ts
+++
@@ -1,6 +0,0 @@
-import { Theme } from 'blockly'
-import '@blockly/theme-modern'
-
-declare module '@blockly/theme-modern' {
-    export default Theme
-}
