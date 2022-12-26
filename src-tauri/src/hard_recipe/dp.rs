d8aaf35054199fafa8c771219039ffa957b3921e
Tnze <cjd001113@outlook.com>
2022-12-26 21:45:01 +0800

    support observe-focused

--- src-tauri/src/hard_recipe/dp.rs
+++ src-tauri/src/hard_recipe/dp.rs
@@ -16,7 +16,7 @@ impl Solver {
     const MAX_GREAT_STRIDES: usize = 3 + 2;
     const MAX_TOUCH_COMBO: usize = 2;
     const MAX_OBSERVE: usize = 1;
-    const TOUCH_SKILLS: [(Actions, u16); 11] = [
+    const TOUCH_SKILLS: [(Actions, u16); 13] = [
         (Actions::BasicTouch, 10),
         (Actions::MastersMend, 0),
         (Actions::StandardTouch, 10),
@@ -28,6 +28,8 @@ impl Solver {
         (Actions::AdvancedTouch, 10),
         (Actions::TrainedFinesse, 0),
         (Actions::Manipulation, 0),
+        (Actions::Observe, 0),
+        (Actions::FocusedTouch, 10),
     ];
 
     pub fn new(init_status: Status) -> Self {
@@ -39,52 +41,55 @@ impl Solver {
         }
     }
 
+    #[rustfmt::skip]
     pub fn read(&mut self, craft_points: i32, durability: u16, buffs: Buffs) -> Option<Actions> {
-        // println!("{craft_points} {durability}");
-        let result = self.results[craft_points as usize
-            * (self.init_status.recipe.durability as usize + 1)
-            + durability as usize][buffs.observed as usize][buffs.touch_combo_stage as usize]
-            [buffs.great_strides as usize][buffs.manipulation as usize][buffs.innovation as usize]
-            [buffs.inner_quiet as usize];
-        match result {
-            _ if craft_points == 0 || durability == 0 => None,
-            Some(action) => Some(action),
-            None => {
-                let mut init_status = self.init_status.clone();
-                init_status.craft_points = craft_points;
-                init_status.durability = durability;
-                init_status.buffs = buffs;
-                let mut best_action = None;
-                let mut best_quality = None;
-                for (action, consumed_du) in Self::TOUCH_SKILLS {
-                    if init_status.is_action_allowed(action).is_ok()
-                        && durability >= init_status.calc_durability(consumed_du)
-                    {
-                        // println!("testing: {:?}", action);
-                        let mut s = init_status.clone();
-                        s.cast_action(action);
-                        while let Some(action) = self.read(s.craft_points, s.durability, s.buffs) {
-                            if let Err(e) = s.is_action_allowed(action) {
-                                panic!("not allowed {:?} on {:?}: {:?}", action, s, e);
-                            }
-                            s.cast_action(action);
-                        }
-                        if s.quality > best_quality.unwrap_or_default() {
-                            best_action = Some(action);
-                            best_quality = Some(s.quality);
-                        }
-                    }
+        if craft_points == 0 || durability == 0 {
+            return None;
+        }
+        if let Some(action) = self.results
+            [craft_points as usize * (self.init_status.recipe.durability as usize + 1) + durability as usize]
+            [buffs.observed as usize]
+            [buffs.touch_combo_stage as usize]
+            [buffs.great_strides as usize]
+            [buffs.manipulation as usize]
+            [buffs.innovation as usize]
+            [buffs.inner_quiet as usize] {
+            return Some(action);
+        }
+        let mut init_status = self.init_status.clone();
+        init_status.craft_points = craft_points;
+        init_status.durability = durability;
+        init_status.buffs = buffs;
+        let mut best_action = None;
+        let mut best_quality = None;
+        for (action, consumed_du) in Self::TOUCH_SKILLS {
+            if init_status.is_action_allowed(action).is_err() ||
+                durability < init_status.calc_durability(consumed_du) ||
+                init_status.success_rate(action) < 100 {
+                continue;
+            }
+            let mut s = init_status.clone();
+            s.cast_action(action);
+            while let Some(action) = self.read(s.craft_points, s.durability, s.buffs) {
+                if let Err(e) = s.is_action_allowed(action) {
+                    panic!("not allowed {:?} on {:?}: {:?}", action, s, e);
                 }
-                // println!("best: {:?}", best_action);
-                self.results[craft_points as usize
-                    * (self.init_status.recipe.durability as usize + 1)
-                    + durability as usize][buffs.observed as usize]
-                    [buffs.touch_combo_stage as usize][buffs.great_strides as usize]
-                    [buffs.manipulation as usize][buffs.innovation as usize]
-                    [buffs.inner_quiet as usize] = best_action;
-                best_action
+                s.cast_action(action);
+            }
+            if s.quality > best_quality.unwrap_or_default() {
+                best_action = Some(action);
+                best_quality = Some(s.quality);
             }
         }
+        self.results
+            [craft_points as usize * (self.init_status.recipe.durability as usize + 1) + durability as usize]
+            [buffs.observed as usize]
+            [buffs.touch_combo_stage as usize]
+            [buffs.great_strides as usize]
+            [buffs.manipulation as usize]
+            [buffs.innovation as usize]
+            [buffs.inner_quiet as usize] = best_action;
+        best_action
     }
 }
 
