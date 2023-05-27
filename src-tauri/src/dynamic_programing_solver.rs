6ee7ada6196ba7c1910e69402b4ec32e9cb2f64c
Tnze <cjd001113@outlook.com>
2023-05-27 15:37:33 +0800

    quality solver fixed

--- src-tauri/src/dynamic_programing_solver.rs
+++ src-tauri/src/dynamic_programing_solver.rs
@@ -1,7 +1,7 @@
 use crate::solver::Solver;
-use ffxiv_crafting::{Actions, Attributes, Recipe, Status};
+use ffxiv_crafting::{Actions, Status};
 use micro_ndarray::Array;
-use std::{cell::Cell, sync::Arc};
+use std::cell::Cell;
 
 #[derive(Clone, Copy, Default)]
 struct SolverSlot<T> {
@@ -38,231 +38,136 @@ const TOUCH_SKILLS: [Actions; 13] = [
     Actions::Manipulation,
 ];
 
-// pub struct QualitySolver<const MN: usize, const WN: usize>
-// where
-//     [[(); WN]; MN]:,
-// {
-//     init_status: Status,
-//     progress_solver: Arc<ProgressSolver<MN, WN>>,
-//     // results [d][cp][iq][iv][gs][mn][wn][touch]
-//     // results: Vec<Vec<[[[[[[SolverSlot<u32>; 3]; WN]; MN]; 4]; 5]; 11]>>,
-//     results: Array<SolverSlot<u32>, 8>,
-// }
+pub struct QualitySolver<'a, const MN: usize, const WN: usize>
+where
+    [[(); WN]; MN]:,
+{
+    init_status: Status,
+    progress_solver: &'a ProgressSolver<MN, WN>,
+    // results [iq][iv][gs][mn][wn][touch][d][cp]
+    results: Array<Cell<Option<SolverSlot<u32>>>, 8>,
+}
 
-// impl<const MN: usize, const WN: usize> QualitySolver<MN, WN>
-// where
-//     [[(); WN]; MN]:,
-// {
-//     pub fn new(init_status: Status, progress_solver: Arc<ProgressSolver<MN, WN>>) -> Self {
-//         let cp = init_status.attributes.craft_points as usize;
-//         let du = init_status.recipe.durability as usize;
-//         Self {
-//             init_status,
-//             progress_solver,
-//             // results: vec![vec![Self::DEFAULT_ARY; cp + 1]; du / 5 + 1],
-//             results: Array::new([du / 5 + 1, cp + 1, 11, 5, 4, MN, WN, 3]),
-//         }
-//     }
+impl<'a, const MN: usize, const WN: usize> QualitySolver<'a, MN, WN>
+where
+    [[(); WN]; MN]:,
+{
+    pub fn new(init_status: Status, progress_solver: &'a ProgressSolver<MN, WN>) -> Self {
+        let cp = init_status.attributes.craft_points as usize;
+        let du = init_status.recipe.durability as usize;
+        Self {
+            init_status,
+            progress_solver,
+            results: Array::new([11, 5, 4, MN + 1, WN + 1, 3, du / 5 + 1, cp + 1]),
+        }
+    }
 
-//     pub(crate) fn get(&self, s: &Status) -> Option<&SolverSlot<u32>> {
-//         self.results
-//             .get(s.durability as usize / 5)?
-//             .get(s.craft_points as usize)?
-//             .get(s.buffs.inner_quiet as usize)?
-//             .get(s.buffs.innovation as usize)?
-//             .get(s.buffs.great_strides as usize)?
-//             .get(s.buffs.manipulation as usize)?
-//             .get(s.buffs.wast_not as usize)?
-//             .get(s.buffs.touch_combo_stage as usize)
-//     }
+    fn get(&self, s: &Status) -> &Cell<Option<SolverSlot<u32>>> {
+        &self.results[[
+            s.buffs.inner_quiet as usize,
+            s.buffs.innovation as usize,
+            s.buffs.great_strides as usize,
+            s.buffs.manipulation as usize,
+            s.buffs.wast_not as usize,
+            s.buffs.touch_combo_stage as usize,
+            s.durability as usize / 5,
+            s.craft_points as usize,
+        ]]
+    }
 
-//     pub(crate) unsafe fn get_unchecked(&self, s: &Status) -> &SolverSlot<u32> {
-//         &self
-//             .results
-//             .get_unchecked(s.durability as usize / 5)
-//             .get_unchecked(s.craft_points as usize)
-//             .get_unchecked(s.buffs.inner_quiet as usize)
-//             .get_unchecked(s.buffs.innovation as usize)
-//             .get_unchecked(s.buffs.great_strides as usize)
-//             .get_unchecked(s.buffs.manipulation as usize)
-//             .get_unchecked(s.buffs.wast_not as usize)
-//             .get_unchecked(s.buffs.touch_combo_stage as usize)
-//     }
-// }
+    fn inner_read(&self, s: &Status) -> SolverSlot<u32> {
+        let slot = self.get(s);
+        if let Some(result) = slot.get() {
+            return result;
+        }
+        if s.durability == 0 {
+            let result = SolverSlot {
+                value: 0,
+                step: 0,
+                action: None,
+            };
+            slot.set(Some(result));
+            return result;
+        }
+        let mut best = SolverSlot {
+            value: 0,
+            step: 0,
+            action: None,
+        };
+        for sk in &TOUCH_SKILLS {
+            match sk {
+                &Actions::Manipulation if MN == 0 => continue,
+                &Actions::WasteNot | &Actions::WasteNotII if WN == 0 => continue,
+                _ => {}
+            }
+            if s.is_action_allowed(*sk).is_err() {
+                continue;
+            }
 
-// impl<const MN: usize, const WN: usize> Solver for QualitySolver<MN, WN>
-// where
-//     [[(); WN]; MN]:,
-// {
-//     fn init(&mut self) {
-//         let mut s = self.init_status.clone();
-//         let progress_solver = &*self.progress_solver;
-//         let difficulty = s.recipe.difficulty;
-//         for cp in 0..=s.attributes.craft_points {
-//             s.craft_points = cp;
-//             for du in (1..=s.recipe.durability).filter(|v| v % 5 == 0) {
-//                 s.durability = du;
-//                 for iq in 0..=MAX_INNER_QUIET {
-//                     s.buffs.inner_quiet = iq;
-//                     for iv in 0..=MAX_INNOVATION {
-//                         s.buffs.innovation = iv;
-//                         for gs in 0..=MAX_GREAT_STRIDES {
-//                             s.buffs.great_strides = gs;
-//                             for mn in 0..MN {
-//                                 s.buffs.manipulation = mn as u8;
-//                                 for wn in 0..WN {
-//                                     s.buffs.wast_not = wn as u8;
-//                                     for touch in 0..3 {
-//                                         s.buffs.touch_combo_stage = touch as u8;
-//                                         for sk in &TOUCH_SKILLS {
-//                                             match sk {
-//                                                 &Actions::Manipulation if MN == 0 => continue,
-//                                                 &Actions::WasteNot | &Actions::WasteNotII
-//                                                     if WN == 0 =>
-//                                                 {
-//                                                     continue
-//                                                 }
+            let mut new_s = s.clone();
+            new_s.quality = 0;
+            new_s.cast_action(*sk);
 
-//                                                 _ => {}
-//                                             }
-//                                             if s.is_action_allowed(*sk).is_err() {
-//                                                 continue;
-//                                             }
-//                                             let mut new_s = s.clone();
-//                                             new_s.cast_action(*sk);
-//                                             unsafe {
-//                                                 let progress =
-//                                                     progress_solver.get_unchecked(&new_s).value;
-//                                                 if new_s.progress + progress >= difficulty {
-//                                                     let mut quality = new_s.quality;
-//                                                     let mut step = 1;
-//                                                     {
-//                                                         let next = self.get_unchecked(&new_s);
-//                                                         quality += next.value;
-//                                                         step += next.step;
-//                                                     }
-//                                                     let slot = self
-//                                                         .results
-//                                                         .get_unchecked_mut(du as usize / 5)
-//                                                         .get_unchecked_mut(cp as usize)
-//                                                         .get_unchecked_mut(iq as usize)
-//                                                         .get_unchecked_mut(iv as usize)
-//                                                         .get_unchecked_mut(gs as usize)
-//                                                         .get_unchecked_mut(mn as usize)
-//                                                         .get_unchecked_mut(wn as usize)
-//                                                         .get_unchecked_mut(touch);
-//                                                     if (quality == slot.value && step < slot.step)
-//                                                         || quality > slot.value
-//                                                     {
-//                                                         *slot = SolverSlot {
-//                                                             value: quality,
-//                                                             step,
-//                                                             action: Some(*sk),
-//                                                         }
-//                                                     }
-//                                                 }
-//                                             }
-//                                         }
-//                                     }
-//                                 }
-//                             }
-//                         }
-//                     }
-//                 }
-//             }
-//         }
-//     }
+            let progress = self.progress_solver.inner_read(&new_s).value;
+            if new_s.progress + progress >= new_s.recipe.difficulty {
+                let mut quality = new_s.quality;
+                let mut step = 1;
+                {
+                    let next = self.inner_read(&new_s);
+                    quality += next.value;
+                    step += next.step;
+                }
+                if (quality == best.value && step < best.step) || quality > best.value {
+                    best = SolverSlot {
+                        value: quality,
+                        step,
+                        action: Some(*sk),
+                    }
+                }
+            }
+        }
+        slot.set(Some(best));
+        best
+    }
+}
 
-//     fn read(&self, s: &Status) -> Option<(Actions, u32)> {
-//         let max_quality = s.recipe.quality;
-//         let mut new_s = s.clone();
-//         let max_addon = max_quality - s.quality;
-//         let mut best = {
-//             let Some(&SolverSlot {
-//                 value: quality,
-//                 step,
-//                 action,
-//             }) = self.get(&s) else {
-//                 return None;
-//             };
-//             let quality = quality.min(max_addon);
-//             ((quality, step), (s.craft_points, s.durability), action)
-//         };
-//         for cp in 0..=s.craft_points {
-//             new_s.craft_points = cp;
-//             for du in 1..=s.durability {
-//                 new_s.durability = du;
-//                 let Some(&SolverSlot {
-//                     value: quality,
-//                     step,
-//                     action: skill,
-//                 }) = self.get(&new_s)else {
-//                     return None;
-//                 };
-//                 let quality = quality.min(max_addon);
-//                 if quality >= best.0 .0 && step < best.0 .1 {
-//                     best = ((quality, step), (cp, du), skill);
-//                 }
-//             }
-//         }
-//         match best.2 {
-//             Some(sk) => Some((sk, best.0 .0)),
-//             None => self.progress_solver.read(&s),
-//         }
-//     }
+impl<'a, const MN: usize, const WN: usize> Solver for QualitySolver<'a, MN, WN>
+where
+    [[(); WN]; MN]:,
+{
+    fn init(&mut self) {}
 
-//     fn read_all(&self, s: &Status) -> Vec<Actions> {
-//         let max_quality = s.recipe.quality;
-//         let mut new_s = s.clone();
-//         let mut list = Vec::new();
-//         loop {
-//             let max_addon = max_quality - new_s.quality;
-//             let mut new_s2 = new_s.clone();
-//             let mut best = {
-//                 let Some(&SolverSlot {
-//                     value: quality,
-//                     step,
-//                     action: skill,
-//                 }) = self.get(&new_s)else {
-//                     return vec![];
-//                 };
-//                 let quality = quality.min(max_addon);
-//                 (
-//                     (quality, step),
-//                     (new_s.craft_points, new_s.durability),
-//                     skill,
-//                 )
-//             };
-//             for cp in 0..=new_s.craft_points {
-//                 new_s2.craft_points = cp;
-//                 for du in 1..=new_s.durability {
-//                     new_s2.durability = du;
-//                     let Some(&SolverSlot {
-//                         value: quality,
-//                         step,
-//                         action: skill,
-//                     }) = self.get(&new_s2) else {
-//                         return vec![];
-//                     };
-//                     let quality = quality.min(max_addon);
-//                     if quality >= best.0 .0 && step < best.0 .1 {
-//                         best = ((quality, step), (cp, du), skill);
-//                     }
-//                 }
-//             }
-//             match best.2 {
-//                 Some(sk) => {
-//                     new_s.cast_action(sk);
-//                     list.push(sk);
-//                 }
-//                 None => {
-//                     list.append(&mut self.progress_solver.read_all(&new_s));
-//                     break list;
-//                 }
-//             }
-//         }
-//     }
-// }
+    fn read(&self, s: &Status) -> Option<Actions> {
+        let max_quality = s.recipe.quality;
+        let mut new_s = s.clone();
+        let max_addon = max_quality - s.quality;
+        let mut best = {
+            let SolverSlot {
+                value: quality,
+                step,
+                action,
+            } = self.inner_read(&s);
+            let quality = quality.min(max_addon);
+            ((quality, step), (s.craft_points, s.durability), action)
+        };
+        for cp in 0..=s.craft_points {
+            new_s.craft_points = cp;
+            for du in 1..=s.durability {
+                new_s.durability = du;
+                let SolverSlot {
+                    value: quality,
+                    step,
+                    action: skill,
+                } = self.inner_read(&new_s);
+                let quality = quality.min(max_addon);
+                if quality >= best.0 .0 && step < best.0 .1 {
+                    best = ((quality, step), (cp, du), skill);
+                }
+            }
+        }
+        best.2.or_else(|| self.progress_solver.read(&s))
+    }
+}
 
 /// ProgressSolver 是一种专注于推动进展的求解器，给定玩家属性和配方并经过初始化后，
 /// 对于任意的当前状态，可以以O(1)时间复杂度算出剩余资源最多可推多少进展。
@@ -284,7 +189,7 @@ where
         let du = init_status.recipe.durability as usize;
         Self {
             init_status,
-            results: Array::new([5, 6, MN, WN, du / 5 + 1, cp + 1]),
+            results: Array::new([5, 6, MN + 1, WN + 1, du / 5 + 1, cp + 1]),
         }
     }
 
@@ -407,6 +312,8 @@ where
 mod test {
     use ffxiv_crafting::{Attributes, Recipe, Status};
 
+    use crate::dynamic_programing_solver::QualitySolver;
+
     use super::ProgressSolver;
     use super::Solver;
 
@@ -431,8 +338,17 @@ mod test {
     #[test]
     fn test() {
         let init_status = init();
-        let mut solver = ProgressSolver::<8, 8>::new(init_status.clone());
-        solver.init();
+        let solver = ProgressSolver::<8, 8>::new(init_status.clone());
+        let actions = solver.read_all(&init_status);
+        println!("{actions:?}");
+    }
+
+    #[test]
+    fn test2() {
+        let mut init_status = init();
+        init_status.cast_action(ffxiv_crafting::Actions::Reflect);
+        let solver = ProgressSolver::<8, 8>::new(init_status.clone());
+        let solver = QualitySolver::<8, 8>::new(init_status.clone(), &solver);
         let actions = solver.read_all(&init_status);
         println!("{actions:?}");
     }
