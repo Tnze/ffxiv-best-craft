c060eb277e53c151305c96a70388dd533cfe116c
Tnze <cjd001113@outlook.com>
2023-06-13 09:18:58 +0800

    mp dfs

--- src-tauri/src/depth_first_search_solver.rs
+++ src-tauri/src/depth_first_search_solver.rs
@@ -1,3 +1,4 @@
+use crossbeam_channel::{Sender, TrySendError};
 use ffxiv_crafting::{Actions, Status};
 
 use crate::solver::Score;
@@ -7,52 +8,85 @@ use crate::solver::Score;
 /// status为开始制作时的初始状态
 /// maximum_depth为限制最深搜索深度
 pub fn solve(status: &Status, maximum_depth: u16, specialist: bool) -> Vec<Actions> {
-    let mut best_list = Vec::new();
-    let mut best_score = Score::from(status);
+    let num = num_cpus::get();
+    let (sender, receiver) = crossbeam_channel::bounded::<(Status, Vec<Actions>)>(num);
+    let (task_sender, task_receiver) = crossbeam_channel::bounded(num);
+    for _ in 0..num {
+        let r = receiver.clone();
+        let s = sender.clone();
+        let ts = task_sender.clone();
+        std::thread::spawn(move || {
+            while let Ok((status, actions)) = r.recv() {
+                ts.send(search(status, actions, &s)).unwrap();
+            }
+        });
+    }
+    sender.send((status.clone(), Vec::new())).unwrap();
 
-    // let (sender, receiver) = crossbeam_channel::bounded(0);
-    // for _ in 0..num_cpus::get() {
-    //     let receiver = receiver.clone();
-    //     std::thread::spawn(move || while let Ok(status) = receiver.recv() {});
-    // }
-    // sender.send(status.clone());
+    let mut best_actions = Vec::new();
+    let mut best_score = Score::from(status);
+    while let Ok((score, actions)) = task_receiver.recv() {
+        if score > best_score {
+            best_actions = actions;
+            best_score = score;
+            println!("{:?}", best_actions);
+        }
+    }
+
+    best_actions
+}
+
+fn search(
+    status: Status,
+    actions: Vec<Actions>,
+    sender: &Sender<(Status, Vec<Actions>)>,
+) -> (Score, Vec<Actions>) {
+    let mut best_actions = actions.clone();
+    let mut best_score = Score::from(&status);
+    let specialist = false;
 
     let mut stack = Vec::new();
-    let mut stack_seq = Vec::new();
+    let mut stack_seq = actions;
     stack.push((status.clone(), SKILL_LIST.into_iter()));
+    stack_seq.push(Actions::BasicSynthesis);
     while let Some((status, action_iter)) = stack.last_mut() {
-        if let Some(next_action) = action_iter.next() {
+        let Some(next_action) = action_iter.next() else {
+            stack.pop().unwrap();
+            stack_seq.pop().unwrap();
+            continue;
+        };
+        *stack_seq.last_mut().unwrap() = next_action;
+
+        if !matches!(next_action, Actions::FocusedSynthesis | Actions::FocusedTouch if status.buffs.observed == 0)
+            && !matches!(next_action, Actions::FinalAppraisal if status.buffs.final_appraisal == 0)
+            && !matches!(next_action, Actions::HeartAndSoul if specialist)
+            && status.is_action_allowed(next_action).is_ok()
+        {
             let mut new_s = status.clone();
-            if !matches!(next_action, Actions::FocusedSynthesis | Actions::FocusedTouch if status.buffs.observed == 0)
-                && !matches!(next_action, Actions::FinalAppraisal if status.buffs.final_appraisal == 0)
-                && !matches!(next_action, Actions::HeartAndSoul if specialist)
-                && new_s.is_action_allowed(next_action).is_ok()
-            {
-                new_s.cast_action(next_action);
-                stack_seq.push(next_action);
-                if !new_s.is_finished()
-                    && !(best_score.quality == status.recipe.quality
-                        && best_score.steps < new_s.step as u16)
-                    && new_s.step < maximum_depth as i32
+            new_s.cast_action(next_action);
+            if !new_s.is_finished() {
+                if best_score.quality != new_s.recipe.quality
+                    || best_score.steps >= new_s.step as u16
                 {
-                    stack.push((new_s, SKILL_LIST.into_iter()));
-                } else {
-                    let score = Score::from(&new_s);
-                    if score > best_score {
-                        best_score = score;
-                        best_list = stack_seq.clone();
-                        println!("{:?}", best_list);
+                    if !sender.is_full() {
+                        let payload = (new_s.clone(), stack_seq.clone());
+                        if sender.try_send(payload).is_ok() {
+                            continue;
+                        }
                     }
-                    stack_seq.pop();
+                    stack.push((new_s, SKILL_LIST.into_iter()));
+                    stack_seq.push(next_action);
+                }
+            } else {
+                let score = Score::from(&new_s);
+                if score > best_score {
+                    best_score = score;
+                    best_actions = stack_seq.clone();
                 }
             }
-        } else {
-            stack_seq.pop();
-            stack.pop();
         }
-        // println!("{stack_seq:?}");
     }
-    best_list
+    (best_score, best_actions)
 }
 
 /// 搜索的技能列表
