2757aeebd797420e4154c2b8ccafea8899244113
Tnze <cjd001113@outlook.com>
2023-07-07 09:07:18 +0800

    fix build error

--- src/components/bill-of-material/Item.vue
+++ src/components/bill-of-material/Item.vue
@@ -1,11 +1,13 @@
 <script lang="ts" setup>
-import { itemInfo } from "../../Craft"
+import { useSettingsStore } from '../../store';
+
+const settingStore = useSettingsStore()
 
 const props = defineProps<{
     item_id: number
 }>()
 
-const { name } = await itemInfo(props.item_id)
+const { name } = await settingStore.getDataSource.itemInfo(props.item_id)
 </script>
 
 <template>
