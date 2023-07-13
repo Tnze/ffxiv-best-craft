241f3db87ddb8b1c19a300ef6ba97fc67b911dd6
Tnze <cjd001113@outlook.com>
2023-07-14 00:59:29 +0800

    fix jobs selection for xivapis

--- src/components/recipe-manager/common.ts
+++ src/components/recipe-manager/common.ts
@@ -3,7 +3,7 @@ import { useDesignerStore } from "../../store"
 
 const designerStore = useDesignerStore()
 
-const jobMaps: { [key: string]: Jobs } = {
+const jobMapsZh: { [key: string]: Jobs } = {
     '木工': Jobs.Carpenter,
     '锻冶': Jobs.Blacksmith,
     '铸甲': Jobs.Armorer,
@@ -13,10 +13,59 @@ const jobMaps: { [key: string]: Jobs } = {
     '炼金': Jobs.Alchemist,
     '烹调': Jobs.Culinarian,
 }
+const jobMapsEn: { [key: string]: Jobs } = {
+    'Woodworking': Jobs.Carpenter,
+    'Smithing': Jobs.Blacksmith,
+    'Armorcraft': Jobs.Armorer,
+    'Goldsmithing': Jobs.Goldsmith,
+    'Leatherworking': Jobs.Leatherworker,
+    'Clothcraft': Jobs.Weaver,
+    'Alchemy': Jobs.Alchemist,
+    'Cooking': Jobs.Culinarian,
+}
+const jobMapsJa: { [key: string]: Jobs } = {
+    '木工': Jobs.Carpenter,
+    '鍛冶': Jobs.Blacksmith,
+    '甲冑': Jobs.Armorer,
+    '彫金': Jobs.Goldsmith,
+    '革細工': Jobs.Leatherworker,
+    '裁縫': Jobs.Weaver,
+    '錬金': Jobs.Alchemist,
+    '調理': Jobs.Culinarian,
+}
+const jobMapsDe: { [key: string]: Jobs } = {
+    'Zimmerer': Jobs.Carpenter,
+    'Grobschmied': Jobs.Blacksmith,
+    'Plattner': Jobs.Armorer,
+    'Goldschmied': Jobs.Goldsmith,
+    'Gerber': Jobs.Leatherworker,
+    'Weber': Jobs.Weaver,
+    'Alchemist': Jobs.Alchemist,
+    'Gourmet': Jobs.Culinarian,
+}
+const jobMapsFr: { [key: string]: Jobs } = {
+    'Menuiserie': Jobs.Carpenter,
+    'Métallurgie': Jobs.Blacksmith,
+    'Armurerie': Jobs.Armorer,
+    'Orfèvrerie': Jobs.Goldsmith,
+    'Tannerie': Jobs.Leatherworker,
+    'Couture': Jobs.Weaver,
+    'Alchimie': Jobs.Alchemist,
+    'Cuisine': Jobs.Culinarian,
+}
+
+export function craftTypeTojobs(craftType: string): Jobs {
+    return jobMapsZh[craftType]
+        ?? jobMapsEn[craftType]
+        ?? jobMapsJa[craftType]
+        ?? jobMapsDe[craftType]
+        ?? jobMapsFr[craftType]
+        ?? Jobs.Culinarian
+}
 
 export const selectRecipe = (recipe: Recipe, recipeLevel: RecipeLevel, requirements: RecipeRequirements, item: Item, craftType: string, simulatorMode: boolean) => {
     designerStore.selectRecipe({
-        job: jobMaps[craftType] ?? Jobs.Culinarian,
+        job: craftTypeTojobs(craftType),
         item,
         recipe,
         recipeLevel,
