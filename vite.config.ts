d358365c8c01b47b45c7e9a494b1b8b21e32b42b
Tnze <cjd001113@outlook.com>
2023-10-28 01:17:49 +0800

    Fix build error, see: https://github.com/vitejs/vite/issues/5676

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vite'
+import { PluginOption, defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import ElementPlus from 'unplugin-element-plus/vite'
 import {
@@ -8,9 +8,30 @@ import {
 import wasm from "vite-plugin-wasm";
 import topLevelAwait from "vite-plugin-top-level-await";
 
+const defineTarget = () => {
+  let config: { env: any; }
+  return <PluginOption>{
+    name: 'define target',
+    configResolved(resolvedConfig) {
+      config = resolvedConfig
+    },
+    transform(code: string, _id: any) {
+      const beReplaced = "import.meta.env.VITE_BESTCRAFT_TARGET"
+      if (code.includes(beReplaced)) {
+        const target = config.env.VITE_BESTCRAFT_TARGET
+        const replacement = JSON.stringify(target)
+        return code.replace(beReplaced, replacement);
+      }
+      return undefined;
+    },
+    enforce: "pre",
+  }
+}
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
+    defineTarget(),
     wasm(),
     topLevelAwait(),
     vue(),
@@ -21,5 +42,5 @@ export default defineConfig({
       baseDir: 'src', // base directory for Vue files
       ftlDir: 'src/assets/locales', // directory with ftl files
     }),
-  ],
+  ]
 })
