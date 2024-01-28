4db44298f5985567544fe35d6cc0641443d7ce40
Tnze <cjd001113@outlook.com>
2024-01-28 09:03:04 +0800

    Set name of author

--- src/components/designer/InitialQualitySetting.vue
+++ src/components/designer/InitialQualitySetting.vue
@@ -1,6 +1,6 @@
 <!-- 
     This file is part of BestCraft.
-    Copyright (C) 2023  <name of author>
+    Copyright (C) 2023  Tnze
 
     BestCraft is free software: you can redistribute it and/or modify
     it under the terms of the GNU Affero General Public License as published
