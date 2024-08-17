23ce45929363541515253d15be9fd2936c196fb6
Tnze <cjd001113@outlook.com>
2024-08-18 03:11:55 +0800

    Add ActionPanel implementation for FCO UI

--- src/fco/pages/Simulator.vue
+++ src/fco/pages/Simulator.vue
@@ -19,37 +19,55 @@
 <script setup lang="ts">
 
 import { NGrid, NGi, NCheckbox, NSelect, NCard, NTabs, NTabPane } from 'naive-ui';
-import CraftingState from '../components/CraftingState.vue';
-import Attributes from '../components/Attributes.vue';
 import ActionQueue from '@/components/designer/ActionQueue.vue';
 import { Actions, Jobs } from '@/libs/Craft';
+import { reactive, markRaw } from 'vue';
+import { Sequence } from '@/components/designer/types';
 
-const slots = [
-    { id: 0, action: Actions.TrainedEye },
-    { id: 0, action: Actions.Groundwork },
-];
+import ActionPanel from '../components/ActionPanel.vue';
+import Attributes from '../components/Attributes.vue';
+import CraftingState from '../components/CraftingState.vue';
+
+const rotation = reactive<Sequence>({
+    slots: [
+        { id: 0, action: Actions.TrainedEye },
+        { id: 1, action: Actions.Groundwork },
+    ],
+    maxid: 2,
+});
+
+function pushAction(action: Actions) {
+    rotation.slots.push(markRaw({ id: rotation.maxid++, action }));
+}
 
 </script>
 
 <template>
-    <n-grid x-gap="20px" y-gap="20px" :cols="2" item-responsive responsive="screen">
+    <n-grid x-gap="20px" y-gap="20px" :cols="12" item-responsive responsive="screen">
         <n-gi :span="2">
             <n-checkbox>自定义配方</n-checkbox>
-            <n-select></n-select>
+        </n-gi>
+        <n-gi :span="2">
             <n-select></n-select>
         </n-gi>
-        <n-gi span="2 m:1">
+        <n-gi :span="4">
+            <n-select></n-select>
+        </n-gi>
+        <n-gi span="12 m:6">
             <CraftingState />
         </n-gi>
-        <n-gi span="1 m:1">
+        <n-gi span="12 m:6">
             <Attributes />
         </n-gi>
-        <n-gi :span="2">
+        <n-gi :span="12">
             <n-card>
-                <ActionQueue :list="slots" :job="Jobs.Carpenter" />
+                <ActionQueue v-model:list="rotation.slots" :job="Jobs.Carpenter" />
             </n-card>
         </n-gi>
-        <n-gi :span="2">
+        <n-gi :span="12">
+            <ActionPanel :job="Jobs.Carpenter"  @clicked-action="pushAction" />
+        </n-gi>
+        <n-gi :span="6">
             <n-tabs>
                 <n-tab-pane name="Random"> </n-tab-pane>
                 <n-tab-pane name="Macro"> </n-tab-pane>
