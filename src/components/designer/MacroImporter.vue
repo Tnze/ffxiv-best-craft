53f8d8c5f7917a475f14f8cb2d2aa051195ca6b7
Tnze <cjd001113@outlook.com>
2024-10-26 15:14:58 +0800

    Support import macros

--- src/components/designer/MacroImporter.vue
+++ src/components/designer/MacroImporter.vue
@@ -25,40 +25,90 @@ import { ref } from 'vue';
 const emits = defineEmits<{
     onRecognized: [actions: Actions[]]
 }>()
-const { $t } = useFluent();
+const fluent = useFluent();
 
+// Create a map converts action names to std Action enum
+const namesToAction: Map<string, Actions> = new Map();
+const actionKeys = Object.values(Actions)
+    .filter(v => !v.endsWith('_fail'));
+for (const bundle of fluent.bundles.value) {
+    for (const action of actionKeys) {
+        const msg = fluent.getMessage(bundle, action.replaceAll('_', '-'))!;
+        namesToAction.set(msg.value as string, action);
+    }
+}
+
+// Textarea input
 const inputText = ref('');
 
+// Start parcing user input text
 function confirm() {
     const input = inputText.value;
+    let result: Actions[];
+
     if (input.trimStart().charAt(0) == "[") {
         try {
-            const result = JSON.parse(input);
-            emits('onRecognized', parseJson(result));
+            result = parseJson(JSON.parse(input))
         } catch (err) {
             ElMessage({
                 type: 'error',
                 showClose: true,
-                message: $t('err-parse-json', { err: String(err) }),
-            })
+                message: fluent.$t('err-parse-json', { err: String(err) }),
+            });
+            return;
+        }
+    } else {
+        result = input
+            .split(/\/[^\s]+|<wait\.\d+>|\n/)
+            .flatMap(v => {
+                if (v.length == 0) {
+                    return [];
+                }
+                v = v.trim()
+                // remove quotation
+                if (v.charAt(0) == '"' && v.charAt(v.length - 1) == '"') {
+                    v = v.substring(1, v.length - 1);
+                }
+                const action = namesToAction.get(v);
+                if (action == undefined) {
+                    return [];
+                }
+                return [action]
+            });
+        if (result.length == 0) {
+            ElMessage({
+                type: 'warning',
+                showClose: true,
+                message: fluent.$t('warn-action-not-found'),
+            });
+            return;
         }
     }
+
+    ElMessage({
+        type: 'success',
+        showClose: true,
+        message: fluent.$t('recognize-success', { n: result.length }),
+    });
+    emits('onRecognized', result);
+    inputText.value = "";
 }
 
+// Parse input as json
 function parseJson(result: any): Actions[] {
     if (!Array.isArray(result)) {
-        throw $t('err-not-an-array');
+        throw fluent.$t('err-not-an-array');
     }
     const validActions = new Set(Object.values(Actions).map(v => v as string));
 
     return result.map(v => {
         if (typeof v != 'string') {
-            throw $t('err-not-a-string', { elem: String(v) })
+            throw fluent.$t('err-not-a-string', { elem: String(v) })
         }
         if (validActions.has(v)) {
             return v as Actions
         } else {
-            throw $t('err-invalid-action', { action: String(v) })
+            throw fluent.$t('err-invalid-action', { action: String(v) })
         }
     })
 }
@@ -70,7 +120,7 @@ function parseJson(result: any): Actions[] {
         :placeholder="$t('auto-recognize')" />
     <el-button type="primary" @click="confirm" :disabled="inputText.length == 0">
         {{ $t('confirm') }}
-        </el-button>
+    </el-button>
 </template>
 
 <style scoped>
@@ -87,4 +137,6 @@ err-parse-json = 尝试解析 JSON 失败：{ $err }
 err-not-an-array = 输入的 JSON 不是一个数组
 err-not-a-string = 元素 { $elem } 不是一个字符串
 err-invalid-action = 未知的技能：{ $action }
+warn-action-not-found = 没有识别到技能
+recognize-success = 识别成功，一共导入了 { $n } 个技能
 </fluent>
